e5f8a26bdb97762553ec9e2bc95a276d70523255
jaehyun <jaehyun739@gmail.com>
2018-04-13 15:33:06 +0900

    hi
    test1

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -4,9 +4,11 @@ public class calculator {
     }
 
     public int sum(int a, int b) {
+
         return a + b;
     }
     public int minus(int a, int b) {
+        
         return a - b;
     }
     public int mul(int a, int b) {
